a88f5584a885150a7682d5633df9cabaec39ee57
Beth Barker <126521173+beth-barker@users.noreply.github.com>
2023-03-27 15:52:45 -0600

    Update artist.sql

---
+++ artist.sql
@@ -0,0 +1,20 @@
+1. INSERT INTO artist
+(name)
+VALUES
+('Taylor Swift'),
+('Big Wild'),
+('Bay Ledges')
+
+2. SELECT * FROM artist
+ORDER BY name DESC
+LIMIT 10;
+
+3. SELECT * FROM artist
+ORDER BY name ASC
+LIMIT 5;
+
+4. SELECT * FROM artist
+WHERE name LIKE 'Black%';
+
+5. SELECT * FROM artist
+WHERE name LIKE '%Black%';
